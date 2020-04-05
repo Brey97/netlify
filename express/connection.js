e8c9c820533b5ab3127d13438aa271cc8d31d263
Brey97 <58863509+Brey97@users.noreply.github.com>
2020-04-05 14:32:36 +0200

    update connection

--- express/connection.js
+++ express/connection.js
@@ -2,6 +2,8 @@ const {MongoClient} = require('mongodb');
 
 async function main(){
     /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
     const uri = "mongodb+srv://Brey97:115075284815Brey@clusterdenzel-jesay.mongodb.net/test?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
